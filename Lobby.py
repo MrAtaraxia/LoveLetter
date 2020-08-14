54d7144812a74f637caab1c7c54f8512e58e2d0b
MrAtaraxia <wchnowski@stamfordct.gov>
2020-08-14 09:46:45 -0400

    'Automatic'

---
+++ Lobby.py
@@ -0,0 +1,19 @@
+"""
+- Have the user connect to this when they turn on.
+
+- Have the user authenticate that they are a user.
+
+- Mostly I want an environment where I can have the users login / logout
+and not have it cause any issues. as it will continue when they log back in.
+
+
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
