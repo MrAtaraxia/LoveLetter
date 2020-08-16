e34e90b071e344992c0e3df00ac7efc6bfd23482
MrAtaraxia <wchnowski@stamfordct.gov>
2020-08-15 21:19:23 -0400

    'Automatic'

--- Lobby.py
+++ Lobby.py
@@ -6,7 +6,9 @@
 - Mostly I want an environment where I can have the users login / logout
 and not have it cause any issues. as it will continue when they log back in.
 
+- Have cred files?
 
+- 
 
 """
 
