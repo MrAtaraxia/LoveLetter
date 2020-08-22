b919255920fca5b35cc45d5c8826f5bb6ddaef2c
MrAtaraxia <wchnowski@stamfordct.gov>
2020-08-22 18:48:24 -0400

    'Automatic'

---
+++ Graphics/basicPygame.py
@@ -0,0 +1,37 @@
+
+
+"""
+basicPygame.py
+Think about adding...
+
+- Cards
+
+- On hover for cards
+
+- Try it as On click, move to top right corner.
+
+- Figure out how to adjust other things when that one is going.
+AKA moving the cards to show that one better.
+
+- Figure out the stupid border thing.
+
+- Look for the Border button thing. I might want that again.
+
+- Find my old dragging around mechanics.
+
+- What graphics do I want to use for this?
+
+
+
+
+
+"""
+import pygame
+
+
+def main():
+    pygame.init()
+
+
+if __name__ == "__main__":
+    main()
